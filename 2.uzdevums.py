66be2d733b6bf18aa8143fcdb9f27de0fb3c78f8
edgarssniedze <edgars.sniedze@skolo.lv>
2023-11-22 17:47:11 +0200

    2.uzd

---
+++ 2.uzdevums.py
@@ -0,0 +1,5 @@
+# 2.uzdevums
+skaitlis = 0 
+while skaitlis**2 < 1000: # cikls, kas kapina skaitli lidz kvadrats ir lielaks par 1000 - tad apstajas
+    skaitlis+=1  # pieskaia klat vienu, ja cikls turpinas
+
